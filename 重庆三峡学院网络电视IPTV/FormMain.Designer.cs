c18b8f1f5e265ea79ad69eac849b6611fd5e0d4f
Comup-X <35429674@qq.com>
2016-01-06 12:29:32 +0800

    init

---
+++ 重庆三峡学院网络电视IPTV/FormMain.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace 重庆三峡学院网络电视IPTV
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.axWindowsMediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer
+            // 
+            this.axWindowsMediaPlayer.Enabled = true;
+            this.axWindowsMediaPlayer.Location = new System.Drawing.Point(-2, 23);
+            this.axWindowsMediaPlayer.Name = "axWindowsMediaPlayer";
+            this.axWindowsMediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer.OcxState")));
+            this.axWindowsMediaPlayer.Size = new System.Drawing.Size(737, 552);
+            this.axWindowsMediaPlayer.TabIndex = 0;
+            // 
+            // comboBox
+            // 
+            this.comboBox.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(61, -1);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(85, 24);
+            this.comboBox.TabIndex = 2;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label.Location = new System.Drawing.Point(3, 3);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(56, 16);
+            this.label.TabIndex = 3;
+            this.label.Text = "频道：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(399, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "因学校原因某些频道不能打开！  By：Mr.Xie";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 571);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.axWindowsMediaPlayer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IPTV";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 重庆三峡学院网络电视IPTV/FormMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace 重庆三峡学院网络电视IPTV
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        void x_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem x = sender as ToolStripMenuItem;
+            string url = "mms://202.202.160.9/" + x.Text;
+            axWindowsMediaPlayer.URL = url;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            string strHTML = "";
+            WebClient myWebClient = new WebClient();
+            Stream myStream = myWebClient.OpenRead("http://vod.sanxiau.edu.cn/IPTV.htm");
+            StreamReader sr = new StreamReader(myStream, System.Text.Encoding.GetEncoding("utf-8"));
+            strHTML = sr.ReadToEnd();
+            myStream.Close();
+            Regex reg = new Regex(@"(?is)<a[^>]*?href=(['""\s]?)(?<href>[^'""\s]*)\1[^>]*?>");
+            MatchCollection match = reg.Matches(strHTML);
+            List<string> url = new List<string>();
+            foreach (Match m in match)
+                url.Add(m.Groups["href"].Value);
+            foreach (string v in url)
+            {
+                char[] temp = v.ToArray();
+                string chT = "";
+                for (int i = temp.Length - 1; i >= 0; i--)
+                {
+                    if (temp[i] != '/')
+                        chT += temp[i];
+                    else
+                        break;
+                }
+                char[] t = chT.ToArray();
+                string ch = "";
+                for (int i = t.Length - 1; i >= 0; i--)
+                    ch += t[i];
+                comboBox.Items.Add(ch);
+                //ToolStripMenuItem x = new ToolStripMenuItem(ch);
+                //x.Click += new EventHandler(x_Click);
+                //choseChannelCToolStripMenuItem.DropDownItems.Add(x);
+            }
+
+            //this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            comboBox.SelectedIndex = 3;
+            axWindowsMediaPlayer.URL = "mms://202.202.160.9/"+comboBox.Items[3];
+            axWindowsMediaPlayer.settings.volume = 100;
+            axWindowsMediaPlayer.enableContextMenu = false;
+            comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox x=sender as ComboBox;
+            string url = "mms://202.202.160.9/" + x.Text;
+            axWindowsMediaPlayer.URL = url;
+        }
+    }
+}
